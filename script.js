c1bc8bad14ac24753797bdf1a02ac81724e71c58
deelite34 <benegeserit@o2.pl>
2022-10-02 20:12:44 +0200

    Decimal counting, merge both buttons into toggle btn

--- script.js
+++ script.js
@@ -4,40 +4,43 @@ window.onload = function initialize() {
 };
 
 function btnPressEvent() {
-  const startBtn = document.querySelector(".start-btn");
-  const stopBtn = document.querySelector(".stop-btn");
+  const toggleTimerBtn = document.querySelector(".toggle-timer-btn");
   const value = document.querySelector(".value");
+  const musicPlayer = document.querySelector(".music-player audio");
   let interval;
-
-  startBtn.addEventListener("click", () => {
-    interval = toggleInterval(value, 1000);
-  });
-  stopBtn.addEventListener("click", () => {
+  let testvar = 0
+  toggleTimerBtn.addEventListener("click", () => {
     interval = toggleInterval(value, 100);
   });
 
   function toggleInterval(element, period) {
     if (!interval) {
-        console.log("increment start")
-      return setInterval((element) => {
-        element.innerHTML = parseInt(element.innerHTML) + 1;
-      }, period, element);
+      console.log("increment start");
+      toggleTimerBtn.innerHTML = "STOP";
+      return setInterval(
+        (element) => {
+          element.innerHTML = (parseFloat(element.innerHTML) + 0.1).toFixed(1)
+        },
+        period, 
+        element
+      );
     }
-    console.log("increment stop")
+    console.log("increment stop");
+    toggleTimerBtn.innerHTML = "START";
     clearInterval(interval);
   }
 }
 
-function setEater(){
-    let txt = document.querySelector(".txt");
-    let choices = ["Patte", "Wrda", "Eduardo", "Deelite", "Kiiskifish", "Mirage"]
-    console.log(choices)
-    
-    let index = Math.floor(Math.random() * choices.length)
-    console.log(choices)
-    console.log(index)
-    console.log(choices[index])
-    let string = `HOW LONG HAS ${choices[index].toUpperCase()} BEEN EATING FOR`
+function setEater() {
+  let txt = document.querySelector(".txt");
+  let choices = ["Patte", "Wrda", "Eduardo", "Deelite", "Kiiskifish", "Mirage"];
+  console.log(choices);
 
-    txt.innerHTML = string;
+  let index = Math.floor(Math.random() * choices.length);
+  console.log(choices);
+  console.log(index);
+  console.log(choices[index]);
+  let string = `HOW LONG HAS ${choices[index].toUpperCase()} BEEN EATING FOR`;
+
+  txt.innerHTML = string;
 }
